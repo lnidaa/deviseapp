789b6b4673c9cbee5deb47ed54714e1dc4b9ebab
lnidaa <daria.narizhna@gotoinc.eu>
2018-07-24 17:51:53 +0300

    Added possibility to publish posts

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Post do
   permit_params :title, :content, :published_at, :user_id
   action_item :publish, only: :show do
+    debugger
     link_to "Publish", publish_admin_post_path(post), method: :put if !post.published_at?
-
-    member_action :publish, method: :put do
-      post = Post.find(params[:id])
-      post.update(published_at: Time.zone.now)
-      redirect_to admin_post_path(post)
-    end
+  end
+  member_action :publish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: Time.zone.now)
+    redirect_to admin_post_path(post)
   end
 end
